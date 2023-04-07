a762cdc1ad4227b65304f0e0f6382f411ef9af2b
Jose Cardenas <hitandrun1310@gmail.com>
2023-04-06 20:30:32 -0400

    Added test cases for account type functionality

--- test_card_system.py
+++ test_card_system.py
@@ -8,25 +8,25 @@ class TestCase(unittest.TestCase):
         self.inFile = sys.argv[1] if len(sys.argv)>1 else ""
         self.clients_limits = {}
         self.clients_accounts = {}
-        self.lines = ["Add John 4111111111111111 $1000", "Charge John $500", "Credit John $100"]
+        self.lines = ["Add John Home 4111111111111111 $1000", "Charge John Home $500", "Credit John Home $100"]
  
     def test_add(self):
-        expected_clients_limits = {'John': 1000}
-        expected_clients_accounts = {'John': 0}
+        expected_clients_limits = {'John_Home': 1000}
+        expected_clients_accounts = {'John_Home': 0}
         card_system.add(self.lines[0])
         self.assertEqual(card_system.clients_limits, expected_clients_limits)
         self.assertEqual(card_system.clients_accounts, expected_clients_accounts)
  
     def test_charge(self):
-        expected_clients_limits = {'John': 1000}
-        expected_clients_accounts = {'John': 500}
+        expected_clients_limits = {'John_Home': 1000}
+        expected_clients_accounts = {'John_Home': 500}
         card_system.charge(self.lines[1])
         self.assertEqual(card_system.clients_limits, expected_clients_limits)
         self.assertEqual(card_system.clients_accounts, expected_clients_accounts)
  
     def test_credit(self):
-        expected_clients_limits = {'John': 1000}
-        expected_clients_accounts = {'John': 400}
+        expected_clients_limits = {'John_Home': 1000}
+        expected_clients_accounts = {'John_Home': 400}
         card_system.credit(self.lines[2])
         self.assertEqual(card_system.clients_limits, expected_clients_limits)
         self.assertEqual(card_system.clients_accounts, expected_clients_accounts)
